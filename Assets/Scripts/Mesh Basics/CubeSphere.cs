95bfd6b0f7edab3623407e94c1d39e2e01f6052b
shi-yh <syhxy008@qq.com>
2022-11-09 16:18:48 +0800

    Cube Sphere

--- Assets/Scripts/Mesh Basics/CubeSphere.cs
+++ Assets/Scripts/Mesh Basics/CubeSphere.cs
@@ -9,7 +9,7 @@ public class CubeSphere : MonoBehaviour
     public int gridSize;
 
     public float radius = 1;
-    
+
     private Mesh _mesh;
 
     private Vector3[] _vertices;
@@ -41,8 +41,8 @@ public class CubeSphere : MonoBehaviour
         _vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
         _normals = new Vector3[_vertices.Length];
         _cubeUV = new Color32[_vertices.Length];
-        
-        
+
+
         int index = 0;
 
 
@@ -52,26 +52,26 @@ public class CubeSphere : MonoBehaviour
         {
             for (int x = 0; x <= gridSize; x++)
             {
-                SetVertex(index++,x, y,0);
+                SetVertex(index++, x, y, 0);
                 yield return _wait;
             }
 
             for (int z = 1; z <= gridSize; z++)
             {
-                SetVertex(index++,gridSize, y,z);
+                SetVertex(index++, gridSize, y, z);
                 yield return _wait;
             }
 
             for (int x = gridSize - 1; x >= 0; x--)
             {
-                SetVertex(index++,x, y,gridSize);
+                SetVertex(index++, x, y, gridSize);
                 yield return _wait;
             }
 
 
             for (int z = gridSize - 1; z > 0; z--)
             {
-                SetVertex(index++,0, y,z);
+                SetVertex(index++, 0, y, z);
                 yield return _wait;
             }
         }
@@ -84,7 +84,7 @@ public class CubeSphere : MonoBehaviour
         {
             for (int x = 1; x < gridSize; x++)
             {
-                SetVertex(index++,x, gridSize,z);
+                SetVertex(index++, x, gridSize, z);
                 yield return _wait;
             }
         }
@@ -93,27 +93,37 @@ public class CubeSphere : MonoBehaviour
         {
             for (int x = 1; x < gridSize; x++)
             {
-                SetVertex(index++,x, 0,z);
+                SetVertex(index++, x, 0, z);
                 yield return _wait;
             }
         }
+
         #endregion
-        
+
         _mesh.vertices = _vertices;
         _mesh.normals = _normals;
         _mesh.colors32 = _cubeUV;
-
     }
 
     private void SetVertex(int index, int x, int y, int z)
     {
+        ///要创建一个单位球体，
+        Vector3 v = new Vector3(x, y, z) / gridSize * 2f - Vector3.one;
 
-        Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
-        _normals[index] = v.normalized;
-        _vertices[index] = _normals[index]*radius;
-        
-        
-        
+        ///这一段数学看不明白，照抄了
+        float x2 = v.x * v.x;
+        float y2 = v.y * v.y;
+        float z2 = v.z * v.z;
+        Vector3 s;
+        s.x = v.x * Mathf.Sqrt(1f - y2 / 2f - z2 / 2f + y2 * z2 / 3f);
+        s.y = v.y * Mathf.Sqrt(1f - x2 / 2f - z2 / 2f + x2 * z2 / 3f);
+        s.z = v.z * Mathf.Sqrt(1f - x2 / 2f - y2 / 2f + x2 * y2 / 3f);
+
+
+        _normals[index] = s;
+        _vertices[index] = _normals[index] * radius;
+
+
         ///顶点颜色组件存储为一个字节，整个颜色的大小为四个字节，与单个浮点数相同
         _cubeUV[index] = new Color32((byte) x, (byte) y, (byte) z, 0);
     }
@@ -122,29 +132,7 @@ public class CubeSphere : MonoBehaviour
     private void GenerateColliders()
     {
         gameObject.AddComponent<SphereCollider>();
-
-
-
     }
-
-
-    private void AddBoxCollider(float x,float y,float z)
-    {
-        BoxCollider c = gameObject.AddComponent<BoxCollider>();
-        c.size = new Vector3(x, y, z);
-    }
-
-    private void AddCapsuleCollider(int direction, float x, float y, float z)
-    {
-        CapsuleCollider c = gameObject.AddComponent<CapsuleCollider>();
-
-        c.center = new Vector3(x, y, z);
-        c.direction = direction;
-        c.height = c.center[direction] * 2f;
-
-
-    }
-    
 
     private IEnumerator GenerateTriangles()
     {
@@ -154,45 +142,53 @@ public class CubeSphere : MonoBehaviour
 
         int ring = (gridSize + gridSize) * 2;
 
-        int v = 0,tZ=0,tX=0,tY=0;
+        int v = 0, tZ = 0, tX = 0, tY = 0;
 
 
         #region 四面包围
-        
-        for (int y = 0; y < gridSize; y++, v++) {
-            for (int q = 0; q < gridSize; q++, v++) {
+
+        for (int y = 0; y < gridSize; y++, v++)
+        {
+            for (int q = 0; q < gridSize; q++, v++)
+            {
                 tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < gridSize; q++, v++) {
+
+            for (int q = 0; q < gridSize; q++, v++)
+            {
                 tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < gridSize; q++, v++) {
+
+            for (int q = 0; q < gridSize; q++, v++)
+            {
                 tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
-            for (int q = 0; q < gridSize - 1; q++, v++) {
+
+            for (int q = 0; q < gridSize - 1; q++, v++)
+            {
                 tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
             }
+
             tX = SetQuad(trianglesX, tX, v, v - ring + 1, v + ring, v + 1);
         }
+
         #endregion
 
         #region 顶部和底部
 
-         tY = CreateTopFace(trianglesY, tY, ring);
+        tY = CreateTopFace(trianglesY, tY, ring);
 
-         tY = CreateBottomFace(trianglesY, tY, ring);
-         
-         
+        tY = CreateBottomFace(trianglesY, tY, ring);
+
         #endregion
 
 
         yield return _wait;
 
-        _mesh.subMeshCount=3;
-        _mesh.SetTriangles(trianglesZ,0);
-        _mesh.SetTriangles(trianglesX,1);
-        _mesh.SetTriangles(trianglesY,2);
-        
+        _mesh.subMeshCount = 3;
+        _mesh.SetTriangles(trianglesZ, 0);
+        _mesh.SetTriangles(trianglesX, 1);
+        _mesh.SetTriangles(trianglesY, 2);
     }
 
     /// <summary>
@@ -208,32 +204,39 @@ public class CubeSphere : MonoBehaviour
         int v = 1;
         int vMid = _vertices.Length - (gridSize - 1) * (gridSize - 1);
         t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
-        for (int x = 1; x < gridSize - 1; x++, v++, vMid++) {
+        for (int x = 1; x < gridSize - 1; x++, v++, vMid++)
+        {
             t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
         }
+
         t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
-        
+
         int vMin = ring - 2;
         vMid -= gridSize - 2;
         int vMax = v + 2;
-        
-        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++) {
+
+        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++)
+        {
             t = SetQuad(triangles, t, vMin, vMid + gridSize - 1, vMin + 1, vMid);
-            for (int x = 1; x < gridSize - 1; x++, vMid++) {
+            for (int x = 1; x < gridSize - 1; x++, vMid++)
+            {
                 t = SetQuad(
                     triangles, t,
                     vMid + gridSize - 1, vMid + gridSize, vMid, vMid + 1);
             }
+
             t = SetQuad(triangles, t, vMid + gridSize - 1, vMax + 1, vMid, vMax);
         }
-        
+
         int vTop = vMin - 1;
         t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
-        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++) {
+        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++)
+        {
             t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
         }
+
         t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
-		
+
         return t;
     }
 
@@ -255,22 +258,27 @@ public class CubeSphere : MonoBehaviour
         int vMin = ring * (gridSize + 1) - 1;
         int vMid = vMin + 1;
         int vMax = start + 2;
-        
-        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++) {
+
+        for (int z = 1; z < gridSize - 1; z++, vMin--, vMid++, vMax++)
+        {
             index = SetQuad(triangles, index, vMin, vMid, vMin - 1, vMid + gridSize - 1);
-            for (int x = 1; x < gridSize - 1; x++, vMid++) {
+            for (int x = 1; x < gridSize - 1; x++, vMid++)
+            {
                 index = SetQuad(
                     triangles, index,
                     vMid, vMid + 1, vMid + gridSize - 1, vMid + gridSize);
             }
+
             index = SetQuad(triangles, index, vMid, vMax, vMid + gridSize - 1, vMax + 1);
         }
-        
+
         int vTop = vMin - 2;
         index = SetQuad(triangles, index, vMin, vMid, vTop + 1, vTop);
-        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++) {
+        for (int x = 1; x < gridSize - 1; x++, vTop--, vMid++)
+        {
             index = SetQuad(triangles, index, vMid, vMid + 1, vTop, vTop - 1);
         }
+
         index = SetQuad(triangles, index, vMid, vTop - 2, vTop, vTop - 1);
 
         #endregion
@@ -288,7 +296,7 @@ public class CubeSphere : MonoBehaviour
         yield return GenerateVertices();
 
         yield return GenerateTriangles();
-        
+
         GenerateColliders();
     }
 
@@ -299,25 +307,5 @@ public class CubeSphere : MonoBehaviour
         triangles[index + 2] = triangles[index + 3] = v10;
         triangles[index + 5] = v11;
         return index + 6;
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        // if (_vertices == null)
-        // {
-        //     return;
-        // }
-        //
-        // Gizmos.color = Color.black;
-        //
-        // for (int i = 0; i < _vertices.Length; i++)
-        // {
-        //     Gizmos.color = Color.black;;
-        //     Gizmos.DrawSphere(_vertices[i], 0.1f);
-        //     
-        //     Gizmos.color = Color.yellow;
-        //     Gizmos.DrawRay(_vertices[i],_normals[i]);
-        // }
     }
 }
