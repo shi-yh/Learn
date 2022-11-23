a5a51096085eec7464f7defb1019c7d5ceba3bc4
shi-yh <syhxy008@qq.com>
2022-11-23 11:20:04 +0800

    board Created

--- Assets/Scripts/Tower Defense/DefenceGameBoard.cs
+++ Assets/Scripts/Tower Defense/DefenceGameBoard.cs
@@ -13,6 +13,9 @@ public class DefenceGameBoard : MonoBehaviour
 
     private DefenceGameTile[] _tiles;
 
+    private Queue<DefenceGameTile> _searchFrontier = new Queue<DefenceGameTile>();
+
+
     public void Initialize(Vector2Int size)
     {
         this._size = size;
@@ -24,14 +27,53 @@ public class DefenceGameBoard : MonoBehaviour
         int index = 0;
         for (int y = 0; y < size.y; y++)
         {
-            for (int x = 0; x < size.x; x++,index++)
+            for (int x = 0; x < size.x; x++, index++)
             {
                 DefenceGameTile tile = _tiles[index] = Instantiate(_tilePrefab);
-                tile.transform.SetParent(transform,false);
-                tile.transform.localPosition = new Vector3(x - offset.x,0, y - offset.y);
+                Transform transform1;
+                (transform1 = tile.transform).SetParent(transform, false);
+                transform1.localPosition = new Vector3(x - offset.x, 0, y - offset.y);
+
+                if (x > 0)
+                {
+                    DefenceGameTile.MakeEastWestNeighbors(tile, _tiles[index - 1]);
+                }
+
+                if (y > 0)
+                {
+                    DefenceGameTile.MakeNorthSouthNeigbors(tile, _tiles[index - size.x]);
+                }
             }
         }
-        
 
+        FindPaths();
+    }
+
+    private void FindPaths()
+    {
+        foreach (DefenceGameTile defenceGameTile in _tiles)
+        {
+            defenceGameTile.ClearPath();
+        }
+
+        _tiles[0].BecomeDestination();
+        _searchFrontier.Enqueue(_tiles[0]);
+
+        while (_searchFrontier.Count > 0)
+        {
+            DefenceGameTile tile = _searchFrontier.Dequeue();
+            if (tile != null)
+            {
+                _searchFrontier.Enqueue(tile.GrowPathNorth());
+                _searchFrontier.Enqueue(tile.GrowPathSouth());
+                _searchFrontier.Enqueue(tile.GrowPathEast());
+                _searchFrontier.Enqueue(tile.GrowPathWest());
+            }
+        }
+
+        foreach (var VARIABLE in _tiles)
+        {
+            VARIABLE.ShowPath();
+        }
     }
 }
--- Assets/Scripts/Tower Defense/DefenceGameTile.cs
+++ Assets/Scripts/Tower Defense/DefenceGameTile.cs
@@ -1,9 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DefenceGameTile : MonoBehaviour
 {
-   [SerializeField]
-   private Transform _arrow = default;
+    [SerializeField] private Transform _arrow = default;
+
+    private DefenceGameTile _north, _east, _south, _west;
+
+
+    #region 寻路相关
+
+    ///下一个瓦片   
+    private DefenceGameTile _nextOnPath;
+
+    /// <summary>
+    /// 离目的地的距离
+    /// </summary>
+    private int _distance;
+
+    public bool HasPath => _distance != int.MaxValue;
+
+    #endregion
+
+    public DefenceGameTile GrowPathNorth() => GrowPathTo(_north);
+    public DefenceGameTile GrowPathEast() => GrowPathTo(_east);
+    public DefenceGameTile GrowPathSouth() => GrowPathTo(_south);
+    public DefenceGameTile GrowPathWest() => GrowPathTo(_west);
+
+    DefenceGameTile GrowPathTo(DefenceGameTile neighbor)
+    {
+        Debug.Assert(HasPath, "No Path");
+
+        if (neighbor == null || neighbor.HasPath)
+        {
+            return null;
+        }
+
+        neighbor._distance = _distance + 1;
+        neighbor._nextOnPath = this;
+        return neighbor;
+    }
+
+
+    /// <summary>
+    /// 在查找路线前，需要初始化路径数据，在找到路径前，还没有下一个瓦片，因此距离可以认为是无限的
+    /// </summary>
+    public void ClearPath()
+    {
+        _distance = int.MaxValue;
+        _nextOnPath = null;
+    }
+
+    /// <summary>
+    /// 当被设置为目的地时，此瓦片距离目的地（自身）的距离为0，且不再有下一个路径
+    /// </summary>
+    public void BecomeDestination()
+    {
+        _distance = 0;
+        _nextOnPath = null;
+    }
+
+    public void ShowPath()
+    {
+        if (_distance == 0)
+        {
+            _arrow.gameObject.SetActive(false);
+            return;
+        }
+
+        _arrow.gameObject.SetActive(true);
+        _arrow.localRotation = _nextOnPath == _north ? northRotation :
+            _nextOnPath == _east ? eastRotation :
+            _nextOnPath == _south ? southRotation :
+            westRotation;
+    }
+
+
+    public static void MakeEastWestNeighbors(DefenceGameTile east, DefenceGameTile west)
+    {
+        Debug.Assert(west._east == null && east._west == null, "Redefined neighbors");
+
+        east._west = west;
+
+        west._east = east;
+    }
+
+    public static void MakeNorthSouthNeigbors(DefenceGameTile north, DefenceGameTile south)
+    {
+        Debug.Assert(south._north == null && north._south == null, "Redefined neighbors");
+
+        south._north = north;
+
+        north._south = south;
+    }
+
+    public static Quaternion
+        northRotation = Quaternion.Euler(90f, 0f, 0f),
+        eastRotation = Quaternion.Euler(90, 90, 0),
+        southRotation = Quaternion.Euler(90, 180, 0),
+        westRotation = Quaternion.Euler(90, 270, 0);
 }
